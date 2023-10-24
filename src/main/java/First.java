60ac6ab5e40fd6bcd6b556a68872913db119f54a
arokosandria <kmsa@interia.pl>
2023-10-24 20:05:13 +0200

    First commit

---
+++ src/main/java/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+}
